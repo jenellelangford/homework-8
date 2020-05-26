db3c2e92d73068647d45044a5c401e2af2260546
Jenelle Langford <jenellelangford@Jenelles-Air.home>
2020-05-26 17:43:03 -0500

    routes updated, notes array rendering, added files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const store = require("../db/store");
+
+
+// I understand what's happening on this page - However, I do
+// not understand the 'res.status(500)' stuff???
+
+
+// GET "/api/notes" responds with all notes from the database
+router.get("/notes", (req, res) => {
+  store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", (req, res) => {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 // Dependencies
 // ===========================================================
 const express = require("express");
-const path = require('path');
-const bodyParser = require('body-parser');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const uuid = require("uuid");
 
 
 const app = express();
-const PORT = 3000;
+const data = ("./db/db.json");
+const PORT = 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'html');
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-// What variables do I need:
-const newTitle = [];
-let newNote = [];
-const deletedNote = [];
 
 
 
@@ -32,51 +30,79 @@ const deletedNote = [];
 
 
 
-app.get('/notes', (req, res) => {
+app.get("/", (req, res) => {
   // should return to notes.html
-  res.sendFile(path.join(__dirname, 'notes.html'));
+  res.sendFile(path.join(__dirname, "/public", "index.html"));
 });
+
+app.get("/notes", (req, res) => {
+  // should return to notes.html
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+
 
 // API Calls
-
-app.get('/api/notes', (req, res) => {
+// Here we get all notes
+app.get("/api/notes", (req, res) => {
+  
   // sould read the db.json file and return all saved notes
-let newNote;
-try { 
-  newNote = fs.readFileSync('db/db.json');
-  newNote = JSON.parse(newNote);
-} catch (err) {
-  console.log(err);
-}
-res.json(newNote);
+  
+  fs.readFile("./db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+    data = JSON.parse(data);
+    res.json(data);
+  });
+  
 });
 
+// Calling one note
+app.get("/api/notes/:id", (req, res) => {
+  res.json(data.filter(notes => notes.id === parseInt(req.params.id)));
+});
+
+/*
+// Here we are creating a note
 app.post('/api/notes', (req, res) => {
 // should recieve new note to save and add to the db.json
-  try {
-    newNote = fs.readFileSync('db/db.json');
-    newNote = JSON.parse;
-    newNote.push(req.body)
+  const createdNote = {title: req.body.title, text: req.body.text};
+  fs.readFile("./db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+    data = JSON.parse(data);
+    data.push(createdNote);
+    data = JSON.stringify(data);
+    fs.writeFile("./db/db.json", data, function (err) {
+      if (err) throw err; 
+    });
 
-    newNote = (JSON.stringify(newNote));
+    res.json(createdNote);
 
-    fs.writeFile('db/db.json')
-  }
-  catch (err) {
-    console.log(err);
-  }
+  });
 });
+*/
+app.post("/api/notes", (req, res) => {
+  const newNote = {
+    id: uuid.v4(),
+    title: req.body.title,
+    text: req.body.text
+  }
 
+  if (!newNote.title || !newNote.title) {
+    res.status(400).json({ msg: "please include title and text"});
+  } 
+  data.push(newNote);
+  res.json(data);
+}); 
+
+
+
+// This should delete notes
 app.delete('/api/notes/:id', (req, res) => {
   // should recieve a query parameter with id of note to delete.
+  
 });
 
 
-app.get('/', (req, res) => {
-  // should return to the index.html
-  res.sendFile(path.join(__dirname, 'index.html'));
-
-});
 
 
 
